320d750362e8a477beacae3820bddb222548f741
prianka0011 <prianka@techintel.com.bd>
2023-09-04 15:12:47 -0500

    Api add

---
+++ E-CommerceApi/E-CommerceApi/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_CommerceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public String getProduct()
+        {
+            return "This method will return list of product";
+        }
+        [HttpGet("{id}")]
+
+        public String getProduct(int id) 
+        {
+            return "This method will return one product";
+        }
+    }
+}
---
+++ E-CommerceApi/E-CommerceApi/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_CommerceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("{id}")]
+
+        public String getTestData(int id)
+        {
+            return "This method will return one Test data";
+        }
+    }
+}
